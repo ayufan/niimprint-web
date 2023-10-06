5a69fa8d4eddf09a98a4c609188bb60fb69f6589
Kamil Trzcinski <ayufan@ayufan.eu>
2023-10-06 21:56:13 +0200

    first commit

---
+++ client.js
@@ -0,0 +1,199 @@
+const BLE_UART_UUID = "49535343-fe7d-4ae5-8fa9-9fafd205e455";
+const BLE_UART_TX_CH_UUID = "49535343-1e4d-4bd9-ba61-23c647249616";
+const BLE_UART_RX_CH_UUID = "49535343-8841-43f4-a8d4-ecbe34729bb3";
+
+const BLE_THERM_UUID = "e7810a71-73ae-499d-8c15-faa9aef0c3f2";
+const BLE_THERM_CH_UUID = "bef8d6c9-9c21-4c9e-b632-bd58c1009f9f";
+
+let options = {
+  filters: [
+    { namePrefix: "D" },
+    // { services: [BLE_UART_UUID] },
+    { services: [BLE_THERM_UUID] }
+  ]
+};
+
+var bluetooth = {};
+
+function to_string(data) {
+  if (typeof data === 'string' || data instanceof String)
+    return data;
+  else if (data instanceof Error)
+    return data.toString();
+  else
+    return JSON.stringify(data);
+}
+
+function log(data) {
+  console.log("LOG: " + to_string(data));
+  const el = document.getElementById('log');
+  if (el) {
+    el.textContent = to_string(data) + "\n" + el.textContent;
+  }
+}
+
+function buffer_to_array(buffer) {
+  let a = [];
+  for (let i = 0; i < buffer.byteLength; i++)
+    a.push(buffer.getUint8(i));
+  return a;
+}
+
+function buffer_to_hex(array, join='') {
+  return array.map(el => ('00' + el.toString(16)).slice(-2)).join(join);
+}
+
+function buffer_to_string(array) {
+  return array.map(el => String.fromCharCode(el)).join('');
+}
+
+function to_packet(type, data) {
+  let packet = [type, data.length].concat(data);
+
+  checksum = 0;
+  packet.forEach(byte => checksum ^= byte);
+
+  // add header: 2x 0x55
+  packet.unshift(0x55);
+  packet.unshift(0x55);
+
+  // add footer: checksum + 2x 0xAA
+  packet.push(checksum);
+  packet.push(0xaa);
+  packet.push(0xaa);
+
+  return packet;
+}
+
+function from_packet(packet) {
+  packet = buffer_to_array(packet);
+
+  if (packet.shift() != 0x55 || packet.shift() != 0x55)
+    return [];
+  if (packet.pop() != 0xAA || packet.pop() != 0xAA)
+    return [];
+
+  checksum = packet.pop();
+  packet.forEach(byte => checksum ^= byte);
+  if (checksum != 0)
+    return [];
+
+  type = packet.shift();
+  len = packet.shift();
+  if (packet.length != len)
+    return [];
+
+  return [type, packet];
+}
+
+async function receive_packet() {
+  return new Promise((resolve, reject) => {
+    log("RX: Waiting for recv.");
+    bluetooth["rx"].addEventListener('characteristicvaluechanged', event => {
+      log("RX: " + buffer_to_array(event.target.value));
+      resolve(from_packet(event.target.value));
+    }, { "once": true });
+
+    setTimeout(() => {
+      reject("RX: timeout");
+    }, 1000);
+  });
+}
+
+async function send_packet(type, data) {
+  const packet = to_packet(type, data);
+  log("TX: Sending... " + packet);
+  return bluetooth["tx"].writeValueWithoutResponse(new Uint8Array(packet))
+    .then(_ => log("TX: Sent."));
+}
+
+async function transceive_packet(type, data, recv_offset = 1) {
+  let process = function([recv_type, recv_data]) {
+    if (recv_type == 219)
+      throw "error";
+    else if (recv_type == 0)
+      throw "not implemented";
+    else if (recv_type == type + recv_offset)
+      return recv_data;
+    else
+      return receive_packet().then(process);
+  };
+
+  let recv = receive_packet().then(process);
+  let send = send_packet(type, data);
+
+  return Promise.all([send, recv]).then(values => values[1]);
+}
+async function connect() {
+  return await navigator.bluetooth.requestDevice(options).then(device => {
+    log(`Name: ${device.name}`);
+    bluetooth["device"] = device;
+
+    log('Connecting to GATT Server...');
+    return device.gatt.connect();
+  }).then(server => {
+    bluetooth["gatt"] = server;
+
+    // Note that we could also get all services that match a specific UUID by
+    // passing it to getPrimaryServices().
+    log('Getting Services...');
+    let allServices = server.getPrimaryServices().then(services => {
+      log('Getting Characteristics...');
+      let queue = Promise.resolve();
+      services.forEach(service => {
+        queue = queue.then(_ => service.getCharacteristics().then(characteristics => {
+          log('> Service: ' + service.uuid);
+          characteristics.forEach(characteristic => {
+            log('>> Characteristic: ' + characteristic.uuid);
+          });
+        }).catch(error => {
+          log('Argh! ' + error);
+        }));
+      });
+    }).catch(error => {
+      log('Argh! ' + error);
+    });
+
+    log('Getting Uart Service...');
+    // let service = server.getPrimaryService(BLE_UART_UUID).then(service => {
+    //   var rx = service.getCharacteristic(BLE_UART_TX_CH_UUID).then(rx => {
+    //     bluetooth["rx"] = rx;
+    //     return tx.startNotifications();
+    //   });
+    //   var tx = service.getCharacteristic(BLE_UART_RX_CH_UUID).then(tx => {
+    //     bluetooth["tx"] = tx;
+    //   });
+    //   return Promise.all([tx, rx]);
+    // }).catch(error => {
+    //   log('Argh! ' + error);
+    // });
+
+    let service = server.getPrimaryService(BLE_THERM_UUID).then(service => {
+      return service.getCharacteristic(BLE_THERM_CH_UUID);
+    }).then(ch => {
+      bluetooth["tx"] = ch;
+      bluetooth["rx"] = ch;
+      return ch.startNotifications();
+    }).catch(error => {
+      log('Argh! ' + error);
+    });
+
+    return Promise.all([service, allServices]);
+  }).then(values => {
+    log("All done: " + values);
+    return bluetooth;
+  }).catch(error => {
+    log('Argh! ' + error);
+    bluetooth = {};
+  });
+}
+
+function disconnect() {
+  if (bluetooth["rx"]) {
+    bluetooth["rx"].stopNotifications();
+  }
+  if (bluetooth["gatt"]) {
+    bluetooth["gatt"].disconnect();
+  }
+  bluetooth = {};
+}
---
+++ cmds.js
@@ -0,0 +1,245 @@
+const INFO_DENSITY = 1;
+const INFO_PRINTSPEED = 2;
+const INFO_LABELTYPE = 3;
+const INFO_LANGUAGETYPE = 6;
+const INFO_AUTOSHUTDOWNTIME = 7;
+const INFO_DEVICETYPE = 8;
+const INFO_SOFTVERSION = 9;
+const INFO_BATTERY = 10;
+const INFO_DEVICESERIAL = 11;
+const INFO_HARDVERSION = 12;
+
+const CMD_GET_INFO = 64;
+const CMD_GET_RFID = 26;
+const CMD_HEARTBEAT = 220;
+const CMD_SET_LABEL_TYPE = 35;
+const CMD_SET_LABEL_DENSITY = 33;
+const CMD_START_PRINT = 1;
+const CMD_END_PRINT = 243;
+const CMD_START_PAGE_PRINT = 3;
+const CMD_END_PAGE_PRINT = 227;
+const CMD_ALLOW_PRINT_CLEAR = 32;
+const CMD_SET_DIMENSION = 19;
+const CMD_SET_QUANTITY = 21;
+const CMD_GET_PRINT_STATUS = 163;
+const CMD_IMAGE_DATA = 133;
+
+// Niimbot D11 has 203 DPI (https://www.niimbotlabel.com/products/niimbot-d11-label-maker)
+function mm_to_px(x) {
+    return Math.ceil(x / 25.4 * 203);
+}
+
+async function get_rfid() {
+  return transceive_packet(CMD_GET_RFID, [1]).then(data => {
+    if (data[0] == 0)
+      return;
+
+    const uuid = data.splice(0, 8);
+    const barcode = data.splice(0, data.shift());
+    const serial = data.splice(0, data.shift());
+    let total_len = data.shift() * 256;
+    total_len += data.shift();
+    let used_len = data.shift() * 256;
+    used_len += data.shift();
+    let type = data.shift();
+    return {
+      'uuid': buffer_to_hex(uuid),
+      'barcode': buffer_to_string(barcode),
+      'serial': buffer_to_string(serial),
+      'total_len': total_len,
+      'used_len': used_len,
+      'type': type
+    };
+  });
+}
+
+async function get_info_type(type) {
+  return transceive_packet(CMD_GET_INFO, [type], type).then(data => {
+    switch (type) {
+    case INFO_DEVICESERIAL:
+      return buffer_to_hex(data);
+
+    // case InfoEnum.SOFTVERSION:
+    //     return _packet_to_int(packet) / 100
+    // case InfoEnum.HARDVERSION:
+    //     return _packet_to_int(packet) / 100
+    // case _:
+    //     return _packet_to_int(packet)
+
+    default:
+      return buffer_to_hex(data);
+    }
+  });
+}
+
+async function get_info() {
+  let sw = get_info_type(INFO_SOFTVERSION);
+  let hw = get_info_type(INFO_HARDVERSION);
+  let sn = get_info_type(INFO_DEVICESERIAL);
+
+  return Promise.all([sw, hw, sn]).then(values => {
+    return {
+      "SW": values[0],
+      "HW": values[1],
+      "S/N": values[2]
+    };
+  });
+}
+
+async function get_heartbeat() {
+  return transceive_packet(CMD_HEARTBEAT, [1]).then(data => {
+    switch (data.length) {
+      case 20:
+        return {
+          'paperstate': data.at(-2),
+          'rfidreadstate': data.at(-1)
+        };
+
+      case 13:
+      case 19:
+        return {
+          'closingstate': data.at(-4),
+          'powerlevel': data.at(-3),
+          'paperstate': data.at(-2),
+          'rfidreadstate': data.at(-1)
+        };
+
+      case 10:
+        return {
+          'closingstate': data.at(-2),
+          'powerlevel': data.at(-1),
+          'rfidreadstate': data.at(-2)
+        };
+
+      case 9:
+        return {
+          'closingstate': data.at(-1)
+        };
+
+      default:
+        return {};
+    }
+  });
+}
+
+async function set_label_type(n) {
+  console.assert(1 <= n && n <= 3);
+  return transceive_packet(CMD_SET_LABEL_TYPE, [n], 16).then(data => data[0]);
+}
+
+async function set_label_density(n) {
+  console.assert(1 <= n && n <= 3);
+  return transceive_packet(CMD_SET_LABEL_DENSITY, [n], 16).then(data => data[0]);
+}
+
+async function start_print() {
+  return transceive_packet(CMD_START_PRINT, [1]).then(data => data[0]);
+}
+
+async function end_print() {
+  return transceive_packet(CMD_END_PRINT, [1]).then(data => data[0]);
+}
+
+async function start_page_print() {
+  return transceive_packet(CMD_START_PAGE_PRINT, [1]).then(data => data[0]);
+}
+
+async function end_page_print() {
+  return transceive_packet(CMD_END_PAGE_PRINT, [1]).then(data => data[0]);
+}
+
+async function allow_print_clear() {
+  return transceive_packet(CMD_ALLOW_PRINT_CLEAR, [1], 16).then(data => data[0]);
+}
+
+async function set_dimension(w, h) {
+  return transceive_packet(CMD_SET_DIMENSION, [
+    Math.floor(w / 256), w % 256,
+    Math.floor(h / 256), h % 256
+  ]).then(data => data[0]);
+}
+
+async function set_quantity(n) {
+  return transceive_packet(CMD_SET_DIMENSION, [Math.floor(n / 256), n % 256]).then(data => data[0]);
+}
+
+async function get_print_status(n) {
+  return transceive_packet(CMD_GET_PRINT_STATUS, [1], 16).then(data => {
+    return {
+      "page": data[0] * 256 + data[1],
+      "progress1": data[2],
+      "progress2": data[3]
+    };
+  });
+}
+
+async function send_line_data(y, w, line_data) {
+  let buffer = [];
+  buffer.push(Math.floor(y / 256));
+  buffer.push(y % 256);
+
+  // add a list of bytes count
+  for (let x = 0; x < w; x += 32) {
+    let bits = 0;
+    for (let b = 0; b < 32; b++)
+      if (line_data[x + b])
+        bits++;
+    buffer.push(bits);
+  }
+  buffer.push(1);
+
+  // encode bool map to bits map
+  for (let x = 0; x < w; x += 8) {
+    let bits = 0;
+    for (let b = 0; b < 8; b++)
+      if (line_data[x + b])
+        bits |= 1<<b;
+    buffer.push(bits);
+  }
+
+  return send_packet(CMD_IMAGE_DATA, buffer).then(_ => new Promise(resolve => setTimeout(resolve, 20)));
+}
+
+async function send_image(w, h, data) {
+  let promise = Promise.resolve();
+
+  for (let y = 0; y < h; y++) {
+    promise = promise.then(_ => {
+      return send_line_data(y, w, data.slice(y * w, (y+1) * w));
+    });
+  }
+
+  return promise;
+}
+
+async function wait_for_quantity(q) {
+  return new Promise((resolve, reject) => {
+    let process = function(status) {
+      if (status["page"] == q)
+        return resolve();
+
+      log(status);
+
+      setTimeout(function() {
+        get_print_status().then(process);
+      }, 0.1);
+    };
+
+    get_print_status().then(process);
+  });
+}
+
+async function print_image(w, h, data, q = 1, type = 1, density = 2) {
+  return set_label_type(type)
+    .then(_ => set_label_type(type)) // 1-3
+    .then(_ => set_label_density(density)) // 1-3
+    .then(_ => start_print())
+    .then(_ => allow_print_clear())
+    .then(_ => start_page_print())
+    .then(_ => set_dimension(w, h))
+    .then(_ => set_quantity(q))
+    .then(_ => send_image(w, h, data))
+    .then(_ => end_page_print())
+    .then(_ => wait_for_quantity(q))
+    .then(_ => end_print());
+}
